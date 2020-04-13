bbe709eb82109f4fe9f58776ea729586c708255d
ianduffield <ianduffield@brandeis.edu>
2020-04-13 11:50:32 -0400

    Added the Cluster Class, expanded the Sample class

---
+++ src/pa06/Cluster.java
@@ -0,0 +1,17 @@
+package pa06;
+
+import java.util.ArrayList;
+/**
+ * A cluster is a cluster point (which is itself a sample)
+ * and a list of the Sample points
+ * @author iandu
+ *
+ */
+public class Cluster {
+
+	public Cluster() {
+		Sample ClusterPoint;
+		ArrayList<Sample> Points;
+	}
+
+}
