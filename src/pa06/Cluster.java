3caafbf5bd3584a2bfb54290b2b3be6001bc1a7c
Eric Hu <62254141+ericwuhu@users.noreply.github.com>
2020-04-19 19:37:30 -0700

    Cluster prints for each line
    Prints each point on it's own line

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -53,7 +53,7 @@ public class Cluster{
 	public void print () {
 		System.out.println ("[");
 		for (int i=0; i< this.SamplePoints.size();i++) {
-			System.out.print("{"+SamplePoints.get(i).getX() +" " + SamplePoints.get(i).getY()+"}, ");
+			System.out.println("{"+SamplePoints.get(i).getX() +" " + SamplePoints.get(i).getY()+"}, ");
 
 		}
 		System.out.print ("{ "+this.ClusterPoint.getX()+ " "+ this.ClusterPoint.getY()+"}, ");
