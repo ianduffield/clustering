05658e40f5cbb1c280f944bee182da273f945745
Ghazalhashemi <57968331+Ghazalhashemi@users.noreply.github.com>
2020-04-20 00:12:13 -0400

    BUG WITH ODD data

--- src/pa06/KMeans.java
+++ src/pa06/KMeans.java
@@ -56,6 +56,7 @@ public class KMeans {
 //			}
 		Km.Reclassify (clustersNum );
 		Km.loop (clustersNum);
+		System.out.println();
 		System.out.println ("The following are the clusters formed. Cluster centers can be found seperated by a comma at the end of the cluster list.");
 		System.out.println();
 		for (int j = 0; j< clustersNum ; j++ ) {
